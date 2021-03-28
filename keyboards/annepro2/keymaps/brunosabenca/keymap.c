417c17456a97f252177cd7246b0abc021016a392
Bruno Sabença <trueheikai@gmail.com>
2021-03-28 11:21:28 +0100

    Swaps 6 and =

--- keyboards/annepro2/keymaps/brunosabenca/keymap.c
+++ keyboards/annepro2/keymaps/brunosabenca/keymap.c
@@ -43,11 +43,11 @@ enum anne_pro_layers {
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_CAW_L] = KEYMAP(
-		KC_ESC,     KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_EQL,  KC_6, KC_7, KC_8,    KC_9,    KC_0,       KC_MINS,    KC_BSPC,
-		KC_TAB,     KC_Q,    KC_W,    KC_F, KC_P, KC_B,   KC_LBRC, KC_J, KC_L, KC_U,    KC_Y,    KC_SCLN,    KC_QUOT,    KC_BSLS,
-		MO(_FNW_L), KC_A,    KC_R,    KC_S, KC_T, KC_G,   KC_RBRC, KC_M, KC_N, KC_E,    KC_I,    KC_O,                   KC_ENT,
-		KC_LSFT,    KC_X,    KC_C,    KC_D, KC_V, KC_Z,   KC_SLSH, KC_K, KC_H, KC_COMM, KC_DOT,  KC_RSFT,
-		KC_LCTL,    KC_LGUI, KC_LALT,             KC_SPC,                               KC_RALT, MO(_FN1_L), MO(_FN2_L), KC_RCTL),
+		KC_ESC,     KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6,    KC_EQL, KC_7, KC_8,    KC_9,    KC_0,       KC_MINS,    KC_BSPC,
+		KC_TAB,     KC_Q,    KC_W,    KC_F, KC_P, KC_B,   KC_LBRC, KC_J,   KC_L, KC_U,    KC_Y,    KC_SCLN,    KC_QUOT,    KC_BSLS,
+		MO(_FNW_L), KC_A,    KC_R,    KC_S, KC_T, KC_G,   KC_RBRC, KC_M,   KC_N, KC_E,    KC_I,    KC_O,                   KC_ENT,
+		KC_LSFT,    KC_X,    KC_C,    KC_D, KC_V, KC_Z,   KC_SLSH, KC_K,   KC_H, KC_COMM, KC_DOT,  KC_RSFT,
+		KC_LCTL,    KC_LGUI, KC_LALT,             KC_SPC,                                 KC_RALT, MO(_FN1_L), MO(_FN2_L), KC_RCTL),
 
     [_QWERTY_L] = KEYMAP(
         KC_ESC,     KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS,    KC_EQL,     KC_BSPC,
