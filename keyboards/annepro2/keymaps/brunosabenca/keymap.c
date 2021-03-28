f695cdbed20e3d1343bef7da9cf7a5b549521a9e
Bruno Sabença <trueheikai@gmail.com>
2021-03-28 11:05:29 +0100

    Adds Colemak variants and Qwerty layers

--- keyboards/annepro2/keymaps/brunosabenca/keymap.c
+++ keyboards/annepro2/keymaps/brunosabenca/keymap.c
@@ -28,33 +28,68 @@ static uint8_t usb_buf[256];
 static uint8_t buf_fil = 0;
 
 enum anne_pro_layers {
-    _BASE_LAYER,
-    _FN1_LAYER,
-    _FN2_LAYER,
+    _COLEMAK_CAW_L,
+    _QWERTY_L,
+    _FN1_L,
+    _FNW_L,
+    _FN2_L,
 };
 
 // This is ROW*MATRIX_COLS + COL
 #define CAPS_LOCATION (MATRIX_COLS * 2 + 0)
+#ifdef _______
+#define _______ KC_TRNS
+#endif
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE_LAYER] = KEYMAP(
-		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
-		KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,
-		MO(_FN1_LAYER), KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT, KC_ENT,
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(_FN1_LAYER), MO(_FN2_LAYER), KC_RCTL),
-    [_FN1_LAYER]  = KEYMAP(
-		KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
-		KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,
-		KC_TRNS, KC_TRNS, KC_END, KC_PGDN, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_VOLD, KC_VOLU, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN2_LAYER), KC_TRNS),
-    [_FN2_LAYER]  = KEYMAP(
-		KC_TRNS, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_TRNS, KC_TRNS, KC_TRNS, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_PROFILE, KC_TRNS, KC_TRNS, KC_TRNS,
-		MO(_FN2_LAYER), KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
-		KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN1_LAYER), MO(_FN2_LAYER), KC_TRNS),
+    [_COLEMAK_CAW_L] = KEYMAP(
+		KC_ESC,     KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_EQL,  KC_6, KC_7, KC_8,    KC_9,    KC_0,       KC_MINS,    KC_BSPC,
+		KC_TAB,     KC_Q,    KC_W,    KC_F, KC_P, KC_B,   KC_LBRC, KC_J, KC_L, KC_U,    KC_Y,    KC_SCLN,    KC_QUOT,    KC_BSLS,
+		MO(_FNW_L), KC_A,    KC_R,    KC_S, KC_T, KC_G,   KC_RBRC, KC_M, KC_N, KC_E,    KC_I,    KC_O,                   KC_ENT,
+		KC_LSFT,    KC_X,    KC_C,    KC_D, KC_V, KC_Z,   KC_SLSH, KC_K, KC_H, KC_COMM, KC_DOT,  KC_RSFT,
+		KC_LCTL,    KC_LGUI, KC_LALT,             KC_SPC,                               KC_RALT, MO(_FN1_L), MO(_FN2_L), KC_RCTL),
+
+    [_COLEMAK_L] = KEYMAP(
+		KC_ESC,     KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS,    KC_EQL,     KC_BSPC,
+		KC_TAB,     KC_Q,    KC_W,    KC_F, KC_P, KC_G,   KC_J, KC_L, KC_U,    KC_Y,   KC_SCLN, KC_LBRC,    KC_RBRC,    KC_BSLS,
+		MO(_FN1_L), KC_A,    KC_R,    KC_S, KC_T, KC_D,   KC_H, KC_N, KC_E,    KC_I,   KC_O,    KC_QUOT,                KC_ENT,
+		KC_LSFT,    KC_Z,    KC_X,    KC_C, KC_V, KC_B,   KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+		KC_LCTL,    KC_LGUI, KC_LALT,             KC_SPC,                              KC_RALT, MO(_FN1_L), MO(_FN2_L), KC_RCTL),
+
+    [_COLEMAK_DH_L] = KEYMAP(
+		KC_ESC,     KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS,    KC_EQL,     KC_BSPC,
+		KC_TAB,     KC_Q,    KC_W,    KC_F, KC_P, KC_B,   KC_J, KC_L, KC_U,    KC_Y,   KC_SCLN, KC_LBRC,    KC_RBRC,    KC_BSLS,
+		MO(_FN1_L), KC_A,    KC_R,    KC_S, KC_T, KC_G,   KC_M, KC_N, KC_E,    KC_I,   KC_O,    KC_QUOT,                KC_ENT,
+		KC_LSFT,    KC_X,    KC_C,    KC_D, KC_V, KC_Z,   KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+		KC_LCTL,    KC_LGUI, KC_LALT,             KC_SPC,                              KC_RALT, MO(_FN1_L), MO(_FN2_L), KC_RCTL),
+
+    [_QWERTY_L] = KEYMAP(
+        KC_ESC,     KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS,    KC_EQL,     KC_BSPC,
+        KC_TAB,     KC_Q,    KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC,    KC_RBRC,    KC_BSLS,
+        MO(_FN1_L), KC_A,    KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,                KC_ENT,
+        KC_LSFT,    KC_Z,    KC_X,    KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_LCTL,    KC_LGUI, KC_LALT,             KC_SPC,                              KC_RALT, MO(_FN1_L), MO(_FN2_L), KC_RCTL),
+
+    [_FN1_L] = KEYMAP(
+		KC_GRV,  KC_F1,            KC_F2,            KC_F3,              KC_F4,         KC_F5,      KC_F6,      KC_F7,   KC_F8,         KC_F9,        KC_F10,                 KC_F11,  KC_F12,  KC_DEL,
+		_______, _______,          KC_HOME,          KC_PGUP,            _______,       _______,    _______,    KC_PSCR, _______,       KC_PSCR,      _______,                _______, _______, _______,
+		_______, _______,          KC_END,           KC_PGDN,            _______,       _______,    KC_LEFT,    KC_DOWN, KC_UP,         KC_RGHT,      _______,                _______, _______,
+		_______, LCTL(KC_Z),       LCTL(KC_X),       LCTL(KC_C),         LCTL(KC_V),    _______,    _______,    _______, _______,       KC_INS,       KC_DEL,                 _______,
+		_______, _______,          _______,          _______,            _______,       _______,    MO(_FN2_L), _______),
+
+    [_FNW_L] = KEYMAP(
+		KC_GRV,  KC_F1,            KC_F2,            KC_F3,              KC_F4,         KC_F5,      KC_F6,      KC_F7,   KC_F8,         KC_F9,        KC_F10,                 KC_F11,  KC_F12,  KC_DEL,
+		_______, _______,          KC_HOME,          KC_PGUP,            _______,       _______,    _______,    _______, KC_PSCR,       _______,      _______,                _______, _______, _______,
+		_______, _______,          KC_END,           KC_PGDN,            _______,       _______,    _______,    KC_LEFT, KC_DOWN,       KC_UP,        KC_RIGHT,               _______, _______,
+		_______, LCTL(KC_Z),       LCTL(KC_X),       LCTL(KC_C),         LCTL(KC_V),    _______,    _______,    _______, _______,       KC_INS,       KC_DEL,                 _______,
+		_______, _______,          _______,          _______,            _______,       _______,    MO(_FN2_L), _______),
+
+    [_FN2_L] = KEYMAP(
+		_______, DF(_COLEMAK_CAW_L),  DF(_QWERTY_L), _______,            _______,       _______,    _______,    _______, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_PROFILE, _______, _______, _______,
+		_______, _______,         KC_UP,             _______,            _______,       _______,    _______,    _______, _______,       _______,      _______,                _______, _______, _______,
+		_______, KC_LEFT,         KC_DOWN,           KC_RGHT,            _______,       _______,    _______,    _______, _______,       _______,      _______,                _______, _______,
+		_______, _______,         _______,           _______,            _______,       _______,    _______,    _______, _______,       _______,      _______,                _______,
+		_______, _______,         _______,           _______,            _______,       MO(_FN1_L), MO(_FN2_L), _______),
 };
 
 const uint16_t keymaps_size = sizeof(keymaps);
